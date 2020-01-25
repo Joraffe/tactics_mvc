95bf8f82e7f295b307f20e3e429c966317158aa2
Jordan Medina <jordan@humblebundle.com>
2020-01-25 14:10:11 -0800

    [Refactor] Ensure Battle prefab uses tile and character prefab

---
+++ Assets/Scripts/SetUp/GameSetUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetUp : MonoBehaviour
+{
+    public GameObject battlePrefab;
+    void Start()
+    {
+        GameObject battle = Instantiate(battlePrefab, Vector3.zero, Quaternion.identity);
+    }
+
+}
